8c1225dba391801be5887588253567a0a26f60d9
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 21:21:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+const Alist=[1,5,2,9,8];
+const map=function(arr,xx){
+  let newarr=[];
+  arr.forEach(element=>{
+    newarr.push(xx(element));
+  });
+  return newarr;
+}
+
+const reduce=function(arr,xx,start){
+  let acc=arr[0];
+  let i;
+  if(!!start)
+  acc +=start;
+  for(i=1;i<arr.length;i++){
+    acc=xx(acc,arr[i])
+  }
+  return acc;
+}
